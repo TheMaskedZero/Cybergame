fb39e3843746f4a3687c7e47cfb4e8d3a910b32a
TheMaskedZero <mike.lien@live.dk>
2022-10-27 14:32:21 +0200

    Throwing le Javelin

---
+++ Assets/Scripts/Javelin.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Javelin : MonoBehaviour
+{
+    //this code was inspired by https://www.youtube.com/watch?v=F20Sr5FlUlE
+
+    [SerializeField] Transform cam, atkPoint;
+    [SerializeField] GameObject javelin;
+
+    [Header("Settings")]
+    //[SerializeField] int totalThrows;
+    [SerializeField] float throwCD;
+
+    [Header("UI")]
+    [SerializeField] Image javelinQUi;
+
+    [Header("Throwing")]
+    [SerializeField] KeyCode throwKey = KeyCode.Q;
+    [SerializeField] float throwForce;
+    [SerializeField] float throwUpForce;
+
+    bool readyToThrow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        readyToThrow = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(throwKey) && readyToThrow)
+        {
+            Throw();
+
+            javelinQUi.fillAmount = 0f;
+        }
+
+        /*if (throwCD < 10)
+        {
+            javelinQUi.fillAmount += (1 * Time.deltaTime) / 10;
+        }*/
+    }
+
+    void Throw()
+    {
+        readyToThrow = false;
+
+        GameObject spear = Instantiate(javelin, atkPoint.position, cam.rotation);
+
+        Rigidbody spearRB = spear.GetComponent<Rigidbody>();
+
+        Vector3 forceToAdd = cam.transform.forward * throwForce + transform.up * throwUpForce;
+
+        spearRB.AddForce(forceToAdd, ForceMode.Impulse);
+
+        //totalThrow --;
+
+        Invoke(nameof(ResetThrow), throwCD);
+    }
+
+    void ResetThrow()
+    {
+        readyToThrow = true;
+    }
+}
